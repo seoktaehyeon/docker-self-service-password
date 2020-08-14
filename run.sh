f267b590955fe9c74e5bed41ad6f2d03d416340f
seoktaehyeon <seoktaehyeon@msn.com>
2020-08-14 13:18:26 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ for kv in $(env | grep ^SSP_)
 do
     k=$(echo "$kv" | awk -F '=' '{print $1}' | awk -F '^SSP_' '{print $2}')
     v=$(echo "$kv" | awk -F '=' '{print $2}')
-    sed -i "s/${k,,}/${v}/" $ssp_conf_file
+    sed -i "s/.*${k,,} .*/\$${k,,} = ${v}/" $ssp_conf_file
 done
 
 httpd-foreground
