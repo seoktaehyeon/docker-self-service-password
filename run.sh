ae1b6d5e91987b19092addb9b501f8d3d7dc4362
seoktaehyeon <seoktaehyeon@msn.com>
2020-08-14 16:12:43 +0800

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ssp_conf_file="/usr/local/apache2/htdocs/conf/conf.inc.php"
+ssp_conf_file="/var/www/html/conf/conf.inc.php"
 
 for kv in $(env | grep ^SSP_)
 do
     k=$(echo "$kv" | awk -F '=' '{print $1}' | awk -F '^SSP_' '{print $2}')
     v=$(echo "$kv" | awk -F '=' '{print $2}')
-    sed -i "s/.*${k,,} .*/\$${k,,} = ${v}/" $ssp_conf_file
+    sed -i "s/{k,,} =.*/${k,,} = '${v}';/" $ssp_conf_file
 done
 
 service apache2 start
