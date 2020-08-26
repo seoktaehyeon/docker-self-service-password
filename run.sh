5295c22ea44b8c78324e30fa5f8fbfc21e1c5a98
seoktaehyeon <seoktaehyeon@msn.com>
2020-08-26 15:46:38 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -12,19 +12,5 @@ else
     cp /opt/config.inc.php ${SSP_CONF}
 fi
 
-# Update config file
-for kv in $(env | grep ^SSP_)
-do
-    k=$(echo "$kv" | awk -F '=' '{print $1}' | awk -F '^SSP_' '{print $2}')
-    v=$(echo "$kv" | awk -F '=' '{print $2}')
-    if [[ "${v}" == "false" ]]; then
-        sed -i "s#${k,,} =.*/${k,,} = false;#" $SSP_CONF
-    elif [[ "${v}" == "true" ]]; then
-        sed -i "s#${k,,} =.*/${k,,} = true;#" $SSP_CONF
-    else
-        sed -i "s#${k,,} =.*/${k,,} = '${v}';#" $SSP_CONF
-    fi
-done
-
 # Start
 docker-php-entrypoint
